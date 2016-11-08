174be1cd9e77a9c62413e131860d98deac776207
lian <lianc@melonbox.local>
2016-11-08 13:28:00 -0500

    update

--- iteration_2/script.js
+++ iteration_2/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() { //always do this first. A special event 
   //append new element 100 times
   for (j = 0; j < 100; j++) { 
-    for (i = 0; i < 20; i++) { 
+    for (i = 0; i < 30; i++) { 
      $("body").append( "<div class='element' style='left:" + i*5
-      + "vw; top:" + j*50 + "px;'></div>" );
+      + "vw; top:" + j*5 + "px;'></div>" );
     console.log("element " + j + ", " + i + " added");
     }
   }
 
 //store random values between 0 to 100, to rx and ry
-var rx = Math.floor(Math.random()*100);
-var ry = Math.floor(Math.random()*100);
+var rx = Math.floor(Math.random()*70);
+var ry = Math.floor(Math.random()*70);
 var color = "#000";
-if (ry < 50) {
+if (ry < 20) {
   //ry is smaller than 50
   color = "yellow";
 }
-else if (rx < 50){
+else if (rx < 20){
   //square is on the left, below the halfway point
   color = "pink";
 }
-else if (rx < 75){
+else if (rx < 40){
   //square on the left, below the half
   color = "orange";
 }
